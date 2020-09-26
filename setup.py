e57edf5b7da9340d094742795a10217f6ff337c8
izztylmaz <izztylmaz1@gmail.com>
2020-09-26 04:09:17 +0300

    Mastermind added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tqa',
+    version='1.0',
+    packages=['gui', 'src', 'src.modelling', 'src.data_receiving', 'src.pre_processing'],
+    url='',
+    license='',
+    author='lapestand',
+    author_email='lapestand@gmail.com',
+    description='Close Domain Q&A System for Turkish'
+)
